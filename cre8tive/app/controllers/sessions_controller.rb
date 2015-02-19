a2c3d3da961f7d8e28ebb0e13bc6f6b8d198a2ee
jarettsims <j@jarettsims.com>
2015-02-19 18:49:10 -0500

    added authenticate to sessions controller

--- cre8tive/app/controllers/sessions_controller.rb
+++ cre8tive/app/controllers/sessions_controller.rb
@@ -1,15 +1,27 @@
 class SessionsController < ApplicationController
+
+	
 	def new
 	end
 
+
 	def create
-		@user = User.find_by(username: params[:username])
-		binding.pry
-		redirect_to "/"
+		user = User.find_by(email: params[:email])
+		if user && user.authenticate(params[:password])
+			session[:user_id] = user.id
+			id = user.id.to_s
+			redirect_to "/user/#{id}"
+		else
+			flash[:error] = 'Invalid email/password'
+			redirect_to '/'
+		end
 	end
+
 
 	def destroy
 		reset_session
 		redirect_to "/"
 	end
+
+
 end
