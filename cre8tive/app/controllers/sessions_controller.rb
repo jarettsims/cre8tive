77255a34220fa3dded4df3e918fb3007e442a381
jarettsims <j@jarettsims.com>
2015-02-19 19:24:46 -0500

    completed sessions with authentication

--- cre8tive/app/controllers/sessions_controller.rb
+++ cre8tive/app/controllers/sessions_controller.rb
@@ -1,16 +1,16 @@
 class SessionsController < ApplicationController
 
-	
+
 	def new
 	end
 
 
 	def create
-		user = User.find_by(email: params[:email])
+		user = User.find_by(username: params[:username])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
 			id = user.id.to_s
-			redirect_to "/user/#{id}"
+			redirect_to "/"
 		else
 			flash[:error] = 'Invalid email/password'
 			redirect_to '/'
